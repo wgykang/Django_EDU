257c6220d1a96bdcd527455e6ee533fb4b89ee97
wgykang <wgykang@qq.com>
2018-06-28 17:08:08 +0800

    完成数据表字段模型设计

---
+++ apps/courses/models.py
@@ -0,0 +1,90 @@
+from datetime import datetime
+
+from django.db import models
+
+from organization.models import Teacher, CourseOrg
+
+
+# 课程基本信息表
+class Course(models.Model):
+    DEGREE_CHOICES = (
+        ("cj", "初级"),
+        ("zj", "中级"),
+        ("gj", "高级")
+    )
+    course_org = models.ForeignKey(CourseOrg, on_delete=models.CASCADE, verbose_name="所属机构", null=True, blank=True)
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE, verbose_name="讲师", null=True, blank=True)
+    name = models.CharField(max_length=50, verbose_name='课程名')
+    desc = models.CharField(max_length=300, verbose_name='课程描述')
+    detail = models.TextField(verbose_name='课程详情')
+    is_banner = models.BooleanField(default=False, verbose_name="是否轮播")
+    degree = models.CharField(choices=DEGREE_CHOICES, max_length=2, verbose_name='难度等级')
+    learn_times = models.IntegerField(default=0, verbose_name='学习时长')
+    students = models.IntegerField(default=0, verbose_name='学习人数')
+    fav_nums = models.IntegerField(default=0, verbose_name='收藏人数')
+    you_need_know = models.CharField(max_length=300, default="一颗勤学的心是本课程必要前提", verbose_name="课程须知")
+    teacher_tell = models.CharField(max_length=300, default="什么都可以学到,按时交作业,不然叫家长", verbose_name="老师告诉你")
+    image = models.ImageField(upload_to='courses/%Y/%m', verbose_name='课程封面图', max_length=100)
+    click_nums = models.IntegerField(default=0, verbose_name='点击量')
+    category = models.CharField(max_length=20, verbose_name="课程类别", default="后端开发")
+    tag = models.CharField(max_length=15, verbose_name="课程标签", default="")
+    add_time = models.DateTimeField(default=datetime.now, verbose_name='添加时间')
+
+    class Meta:
+        verbose_name = '课程信息'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.name
+
+
+# 章节信息表
+class Lesson(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, verbose_name='课程')
+    name = models.CharField(max_length=100, verbose_name='章节名')
+    add_time = models.DateTimeField(default=datetime.now, verbose_name='添加时间')
+
+    class Meta:
+        verbose_name = '章节信息'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.name
+
+
+# 视频信息表
+class Video(models.Model):
+    lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE, verbose_name="章节")
+    url = models.CharField(max_length=200, default="http://www.baidu.com/", verbose_name="访问地址")
+    name = models.CharField(max_length=100, verbose_name="视频名")
+    learn_times = models.IntegerField(default=0, verbose_name="学习时长(分钟数)")
+    add_time = models.DateTimeField(default=datetime.now, verbose_name="添加时间")
+
+    class Meta:
+        verbose_name = u"视频"
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return '{0}章节的视频 >> {1}'.format(self.lesson, self.name)
+
+
+# 课程资源下载表
+class CourseResource(models.Model):
+    # 因为一个课程对应很多资源。所以在课程资源表中将课程设置为外键。
+    # 作为一个字段来让我们可以知道这个资源对应那个课程
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, verbose_name=u"课程")
+    name = models.CharField(max_length=100, verbose_name="名称")
+    # 这里定义成文件类型的field，后台管理系统中会直接有上传的按钮。
+    # FileField也是一个字符串类型，要指定最大长度。
+    download = models.FileField(
+        upload_to="courses/resource/%Y/%m",
+        verbose_name="资源文件",
+        max_length=100)
+    add_time = models.DateTimeField(default=datetime.now, verbose_name="添加时间")
+
+    class Meta:
+        verbose_name = "课程资源"
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return '{0}》课程的资源: {1}'.format(self.course, self.name)
---
+++ apps/users/models.py
@@ -0,0 +1,53 @@
+from datetime import datetime
+
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+# 用户信息模型
+class UserProfile(AbstractUser):
+    nick_name = models.CharField(max_length=64, default='', verbose_name='昵称')
+    birthday = models.DateField(null=True, blank=True, verbose_name='生日')
+    gender = models.CharField(max_length=7, choices=(('male', '男'), ('female', '女')), default='female',
+                              verbose_name="性别")
+    address = models.CharField(max_length=100, default='', verbose_name='地址')
+    mobile = models.CharField(max_length=11, null=True, blank=True, verbose_name='电话')
+    image = models.ImageField(upload_to='image/%Y/%m', default='image/default.png', max_length=100, verbose_name='头像')
+
+    class Meta:
+        verbose_name = '用户信息'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.username
+
+
+# 邮箱验证码
+class EmailVerifyRecord(models.Model):
+    code = models.CharField(max_length=20, verbose_name='验证码')
+    email = models.EmailField(max_length=50, verbose_name='邮箱')
+    send_type = models.CharField(choices=(('register', '注册'), ('forget', '找回密码')), max_length=10, verbose_name='验证码类型')
+    send_time = models.DateTimeField(default=datetime.now)
+
+    class Meta:
+        verbose_name = '邮箱验证码'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return '{}{}'.format(self.code, self.email)
+
+
+# 轮播图
+class Banner(models.Model):
+    title = models.CharField(max_length=100, verbose_name='轮播标题')
+    image = models.ImageField(upload_to='banner/%Y/%m', verbose_name='轮播图', max_length=100)
+    url = models.URLField(max_length=200, verbose_name='访问地址')
+    index = models.IntegerField(default=100, verbose_name='轮播顺序')
+    add_time = models.DateTimeField(default=datetime.now, verbose_name='添加时间')
+
+    class Meta:
+        verbose_name = '轮播图'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return '{}{}'.format(self.title, self.index)
